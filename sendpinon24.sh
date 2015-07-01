15fa2a4b85c58acf07aa49815dd6a98d11edd864
Alessio Garzi <gun101@email.it>
2015-07-01 10:31:19 +0200

    Set connection timeout on curl
    This is necessary if there's no response from the other side so the lock file is removed

--- sendpinon24.sh
+++ sendpinon24.sh
@@ -2,11 +2,11 @@
 (
  	# Wait for lock on /var/lock/.myscript.exclusivelock (fd 200) for 10 seconds
 	flock -x -w 0 200 || exit 1
-	curl $HTTP_URL
+	curl --connect-timeout 10 $HTTP_URL
         if [ $? != 0 ]; then
-        	curl $HTTP_URL 1>/tmp/log1 2>/tmp/log2
+        	curl --connect-timeout 10 $HTTP_URL 1>/tmp/log1 2>/tmp/log2
         	if [ $? != 0 ]; then
-        		curl $HTTP_URL 1>/tmp/log1 2>/tmp/log2
+        		curl --connect-timeout 10 $HTTP_URL 1>/tmp/log1 2>/tmp/log2
         	fi
         fi
         sleep 60;
